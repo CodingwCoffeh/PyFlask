14e557cc082c2b07bd90ddfe64e208995f98cd61
CodingwCoffeh <126407643+CodingwCoffeh@users.noreply.github.com>
2025-12-12 14:07:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -270,5 +270,5 @@ def download_file(filename):
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+if _name_ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
